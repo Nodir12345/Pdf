0e796a9031384c56e52ca9b228d9de771edf9e00
Nodir12345 <nodirxaydarov911@gmail.com>
2023-02-23 18:25:53 +0500

    pdf site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PDFFile from './components/PDFFile';
+// import { PDFDownloadLink } from '@react-pdf/renderer';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
